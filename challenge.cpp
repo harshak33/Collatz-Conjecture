d774768a645b9bdfe89000e8696a85f7515bb629
Sri Harsha Komannuthala <146465450+harshak33@users.noreply.github.com>
2024-06-18 12:48:26 +0530

    first commit
    first commit

---
+++ challenge.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+int main()
+{
+    unsigned long long int a,b;
+    for(int i =1;;i++)
+    {
+        std::cout<<"Log "<<i<<":";
+        a = i;b = 0;
+        while(a!=4)
+        {
+            if(a%2 == 0)
+                a/=2;
+            else
+                a = a*3+1;
+            b++;
+        }
+        std::cout<<b<<"-iterations"<<std::endl;
+    }
+}
